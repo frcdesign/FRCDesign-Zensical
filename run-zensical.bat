6b7dda501bf2b4973a7c6a10326fad7bb23f7924
Andrew Card <andrewccard04@gmail.com>
2025-11-06 14:51:47 -0800

    init

---
+++ run-zensical.bat
@@ -0,0 +1,16 @@
+@echo off
+
+setlocal
+
+set "ROOT=%~dp0"
+
+pushd "%ROOT%"
+
+set "PY=%ROOT%.venv\Scripts\python.exe"
+
+"%PY%" -m zensical serve %*
+
+set "EXITCODE=%ERRORLEVEL%"
+
+popd
+exit /b %EXITCODE%
